71f6a9f1da4b81f24551e8d578eab41578667cee
JungNguyen <111296639+leonx04@users.noreply.github.com>
2023-06-03 13:00:11 +0700

    Add files via upload

---
+++ SinhVien.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package B12;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author dungn
+ */
+public class SinhVien implements Serializable {
+    private int maSinhVien;
+    private String tenSinhVien;
+    private String chuyenNganh;
+    private int soTinChi;
+
+    public int getMaSinhVien() {
+        return maSinhVien;
+    }
+
+    public void setMaSinhVien(int maSinhVien) {
+        this.maSinhVien = maSinhVien;
+    }
+
+    public String getTenSinhVien() {
+        return tenSinhVien;
+    }
+
+    public void setTenSinhVien(String tenSinhVien) {
+        this.tenSinhVien = tenSinhVien;
+    }
+
+    public String getChuyenNganh() {
+        return chuyenNganh;
+    }
+
+    public void setChuyenNganh(String chuyenNganh) {
+        this.chuyenNganh = chuyenNganh;
+    }
+
+    public int getSoTinChi() {
+        return soTinChi;
+    }
+
+    public void setSoTinChi(int soTinChi) {
+        this.soTinChi = soTinChi;
+    }
+}
